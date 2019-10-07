3dfa29c52ff3764955061403cc96e220d70ca2c3
Ronny Alvarado <rsalvarado777@gmail.com>
2019-10-07 13:54:20 -0500

    MVP complete

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
 import { ProductContext } from "./contexts/ProductContext.js";
+import { CartContext } from "./contexts/CartContext.js";
 
 function App() {
   const [products] = useState(data);
@@ -25,12 +26,12 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
-
-        {/* Routes */}
-        <Route exact path="/" component={Products} />
-
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={cart}>
+          <Navigation />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
